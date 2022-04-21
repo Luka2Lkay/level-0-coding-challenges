b76f76751c987f223269046f9271afd79322756e
Luka <lukhanyo95@gmail.com>
2022-04-21 09:00:11 +0200

    returns temperatures instead of printing them

--- task07.js
+++ task07.js
@@ -1,12 +1,14 @@
 function toFahrenheit(celsius) {
-  const temperature = (celsius * 9/5) + 32
+  const temperature = (celsius * 9) / 5 + 32;
 
-  console.log(temperature)
+  return temperature;
 }
 
 function toCelsius(fahrenheit) {
-  const temperature = (fahrenheit - 32) * 5/9
+  const temperature = ((fahrenheit - 32) * 5) / 9;
 
-  console.log(temperature)
+  return temperature;
 }
 
+
+console.log(toFahrenheit(50))
