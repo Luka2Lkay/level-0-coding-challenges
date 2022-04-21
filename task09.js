237c05f8dbfb51e1afe439c0f35ffe54143a8393
Luka <lukhanyo95@gmail.com>
2022-04-21 09:04:16 +0200

    Time variables have been declared with a correct keyword

--- task09.js
+++ task09.js
@@ -1,21 +1,18 @@
+const vowels = ["a", "e", "i", "o", "u"];
 
+function vowelsInString(str) {
+  const empty = [];
 
-const vowels = ['a', 'e', 'i','o','u']
+  let newStr = str.toLowerCase();
 
-function vowelsInString (str){
-
-    const empty = []
-
-    let newStr = str.toLowerCase()
-
-    for (let alphabet of newStr){
-        if (vowels.includes(alphabet)){
-            if(!empty.includes(alphabet) ){
-                empty.push(alphabet)
-            }
-        }
+  for (let alphabet of newStr) {
+    if (vowels.includes(alphabet)) {
+      if (!empty.includes(alphabet)) {
+        empty.push(alphabet);
+      }
     }
+  }
 
-const convertedList = empty.toString()
-console.log(`Vowels: ${convertedList}`)
+  const convertedList = empty.toString();
+  console.log(`Vowels: ${convertedList}`);
 }
