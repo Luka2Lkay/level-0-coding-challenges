14fe6952402d91818f23619647bc874f81bdfc67
Luka <lukhanyo95@gmail.com>
2022-04-21 13:32:28 +0200

    prints vowels instead of returning them

--- task09.js
+++ task09.js
@@ -1,17 +1,18 @@
 function vowelsInString(str) {
   const vowels = ["a", "e", "i", "o", "u"];
-  const empty = [];
+  const empty = []
 
-  const newStr = str.toLowerCase();
+  const newStr = str.toLowerCase()
 
   for (let alphabet of newStr) {
     if (vowels.includes(alphabet)) {
       if (!empty.includes(alphabet)) {
-        empty.push(alphabet);
+        empty.push(alphabet)
       }
     }
   }
 
-  const convertedList = empty.toString();
-  return `Vowels: ${convertedList}`;
+  const convertedList = empty.toString()
+  console.log(`Vowels: ${convertedList}`)
 }
+
