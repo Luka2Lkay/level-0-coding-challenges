3ffbf408e54f30effec741bc459abca7fad36ff3
Luka <lukhanyo95@gmail.com>
2022-04-21 08:55:57 +0200

    returns a maximum number instead of printing it

--- task06.js
+++ task06.js
@@ -1,9 +1,9 @@
 function maximum(a, b, c) {
   if ((a > b) & (a > c)) {
-    console.log(a);
+    return a;
   } else if ((b > a) & (b > c)) {
-    console.log(b);
+    return b;
   } else {
-    console.log(c);
+    return c;
   }
 }
